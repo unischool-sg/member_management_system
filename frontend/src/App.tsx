e53106110c034e3e9880db3b27f4d1ea748f0d70
tanahiro2010 <herentongkegu087@gmail.com>
2026-02-16 20:19:21 +0900

    fix: ワークフローのデプロイ設定にインデントを追加し、修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Router } from 'react-router-dom'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
