becb9a019ca8c6cca743e19b83620a849765c8d1
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-01 14:15:46 +0900

    refactor: ルーティングの型定義を整理し、レイアウトおよびページルートを明確に分離

--- frontend/src/types/routes.ts
+++ frontend/src/types/routes.ts
@@ -1,15 +1,26 @@
-type BaseRoute = {
-    path: string
-    element: React.ReactNode
-    layout?: React.ReactNode
-    meta?: {
-        title: string
-        description: string
-    }
-}
+import type React from "react";
 
-type HasChildrenRoute = BaseRoute & {
-    children: BaseRoute[]
-}
+type Meta = {
+  title?: string;
+  description?: string;
+};
 
-export type Route = BaseRoute | HasChildrenRoute
+type BaseRoute<M> = {
+  path: string;
+  meta?: M;
+};
+
+type LayoutRoute<M = Meta> = BaseRoute<M> & {
+  type: "layout";
+  layout: React.ReactNode;
+  children?: Route<M>[];
+};
+
+type PageRoute<M = Meta> = BaseRoute<M> & {
+  type: "page";
+  element: React.ReactNode;
+  children?: Route<M>[];
+};
+
+export type Route<M = any> = LayoutRoute<M> | PageRoute<M>;
+export type { LayoutRoute, PageRoute };
