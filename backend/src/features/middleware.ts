34be50b81116d25399a9dc879cbcb689569d3191
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-01 17:16:00 +0900

    feat: 認証トークンをヘッダーから取得する関数を追加

--- backend/src/features/middleware.ts
+++ backend/src/features/middleware.ts
@@ -1,9 +1,18 @@
 import { Context, Next } from "hono";
 import type { Env } from "../index";
 import { verifyToken } from "../lib/utils/jwt";
+import { getCookie } from "hono/cookie";
+
+const getTokenFromHeader = (c: Context<Env>): string | null => {
+    const authHeader = c.req.header('Authorization');
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        return authHeader.replace('Bearer ', '');
+    }
+    return getCookie(c, 'authToken') ?? null; // クッキーからトークンを取得
+}
 
 const middleware = async (c: Context<Env>, next: Next) => {
-    const token = c.req.header()['Authorization']?.replace('Bearer ', '');
+    const token = getTokenFromHeader(c);
     const isAuthed = token ? await verifyToken(token, c.env.JWR_SECRET ?? "DEFAULT_JWT_TOKEN") : null;
     c.set('isAuthed', isAuthed);
 
