6a4983f93a5e5998669c29bc7b3dc6c87023104d
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-01 17:16:51 +0900

    feat: UUID生成とランダム文字列生成機能を追加

---
+++ backend/src/lib/utils/key.ts
@@ -0,0 +1,22 @@
+const uuid4 = (): string => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+const randomString = (length: number): string => {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        result += chars[randomIndex];
+    }
+    return result;
+}
+
+const generateKey = (prefix: string, length: number = 16, connectChar: string = "_"): string => {
+    return `${prefix}${connectChar}${randomString(length)}`;
+}
+
+export { uuid4, randomString, generateKey };
