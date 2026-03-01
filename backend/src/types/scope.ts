7a5810eb2f0aa30636dafe8bdf26d3d383d71fb8
tanahiro2010 <herentongkegu087@gmail.com>
2026-03-01 17:18:06 +0900

    feat: スコープタイプを定義する新しいファイルを追加

---
+++ backend/src/types/scope.ts
@@ -0,0 +1,32 @@
+type Scope = 
+    "read_profile"  |
+    "write_profile" |
+    
+    "read_members"  |
+    "write_members" |
+    
+    "read_events"   |
+    "write_events"  |
+
+    "read_teams"    |
+    "write_teams"   |
+
+    "read_surveys"  |
+    "write_surveys" |
+
+    "read_logs"     |
+    "write_logs"    |
+
+    "read_invites"  |
+    "write_invites" |
+
+    "read_announcements"  |
+    "write_announcements" |
+
+    "send_emails"   |
+
+
+    "read_api_keys"  |
+    "write_api_keys";
+
+export type { Scope };
